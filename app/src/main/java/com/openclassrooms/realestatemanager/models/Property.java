f9cd1fc084f6acbf5523827e92e75a7abc349c1a
JFV34 <jfr.vincler@live.fr>
2020-02-17 12:44:56 +0100

    model Property

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/models/Property.java
@@ -0,0 +1,68 @@
+package com.openclassrooms.realestatemanager.models;
+
+public class Property {
+
+    private int id;
+    private String type;
+    private int city;
+    private int price;
+    private String description;
+}
+
+    public Property() {
+    }  //  ?
+
+    public Property(int id, String type, int city, int price, String description) {
+        this.id = id;
+        this.type = type;
+        this.city = city;
+        this.price = price;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCity() {
+        return city;
+    }
+
+    public void setCity(int city) {
+        this.city = city;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+}
+
+
+
+
+
