e7f65b81d7fd5a085dd7e419c9238ba38a4601fd
JFV34 <jfr.vincler@live.fr>
2020-03-15 08:46:42 +0100

    display fragment_insert

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/InsertPropertyFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/InsertPropertyFragment.java
@@ -1,7 +1,6 @@
 package com.openclassrooms.realestatemanager.ui;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,8 +9,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.openclassrooms.realestatemanager.R;
-
-import static android.util.Log.i;
 
 public class InsertPropertyFragment extends Fragment {
 
@@ -28,8 +25,7 @@ public class InsertPropertyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        //View root = inflater.inflate(R.layout.fragment_insert, container, false);
-        View root = null;
+        View root = inflater.inflate(R.layout.fragment_insert, container, false);
 
         return root;
     }
