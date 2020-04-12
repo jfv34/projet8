1632be6b3666769be3050f00f156429403607543
JFV34 <jfr.vincler@live.fr>
2020-04-12 23:23:10 +0200

    floating button for add property in main, and seach button

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/main/MainFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/main/MainFragment.java
@@ -114,6 +114,4 @@ public class MainFragment extends Fragment implements OnPropertyClickedListener 
             transaction.replace(R.id.activity_main_frame_layout_detail_large_screen, searchFragment).commit();
         } else {transaction.replace(R.id.frame_layout_main, searchFragment).commit();}
     }
-
-
     }
