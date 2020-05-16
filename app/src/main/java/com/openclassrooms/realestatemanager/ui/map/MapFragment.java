47a06385be386024a6d6744da5b45f6d9bfad16f
JFV34 <jfr.vincler@live.fr>
2020-05-16 13:31:47 +0200

    display empty map

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/map/MapFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/map/MapFragment.java
@@ -1,11 +1,27 @@
 package com.openclassrooms.realestatemanager.ui.map;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.openclassrooms.realestatemanager.R;
 
 public class MapFragment extends Fragment {
 
     public static MapFragment newInstance() {
         return new MapFragment();
+    }
+
+    private static final int PERMISSIONS_REQUEST_CODE = 123;
+    private GoogleMap googleMap;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
     }
 
 
