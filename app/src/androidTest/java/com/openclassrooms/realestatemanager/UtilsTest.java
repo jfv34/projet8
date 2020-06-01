5980c84db0db2e9ab9227c4e13de6dd52991ecd4
JFV34 <jfr.vincler@live.fr>
2020-06-01 19:29:07 +0200

    -

--- app/src/androidTest/java/com/openclassrooms/realestatemanager/UtilsTest.java
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/UtilsTest.java
@@ -1,13 +1,9 @@
 package com.openclassrooms.realestatemanager;
 
 import android.content.Context;
-
 import androidx.test.platform.app.InstrumentationRegistry;
-
 import org.junit.Test;
-
 import static org.junit.Assert.assertTrue;
-//import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 
 public class UtilsTest {
     @Test
